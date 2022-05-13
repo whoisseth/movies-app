353eff809e9c5622c69577189b865e3a83c64baa
utkarsh <55540613+whoisseth@users.noreply.github.com>
2022-05-13 20:27:28 +0530

    remove recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
